ffde40d8345b628dc49cd8b67bd45b26f541c748
neeraj-gs <gsneeraj05@gmail.com>
2023-08-18 18:17:52 +0530

    Started with the Home COmpoent

--- earth-cart/src/App.jsx
+++ earth-cart/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home.jsx'
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
