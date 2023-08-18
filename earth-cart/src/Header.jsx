3de7657020174131e44a6d71215b91f7e240f677
neeraj-gs <gsneeraj05@gmail.com>
2023-08-18 18:15:12 +0530

    Created a Basic Nvbar without a logo of website

--- earth-cart/src/Header.jsx
+++ earth-cart/src/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
     <div className='header'>
         <img className='header_logo' src="" alt="Earth Cart Logo" />
         <div className="header_search">
-            <input type="text" className="header_search" />
-            <SearchIcon className='header_searchIcon'></SearchIcon>
+            <input type="text" className="header_search_input"  />
+            <SearchIcon className='header_searchIcon' style={{color:'#69863C'}}></SearchIcon>
         </div>
+
         <nav className="header_nav">
           <div className='header_option'>
             <span className="option_line1">
@@ -28,8 +30,10 @@ function Header() {
             </span>
             
           </div>
-          <div className='header_option'>
-            {/* Cart Image */}
+          <div className='header_option_basket'>
+          <ShoppingBasketIcon className='header_cart_logo' style={{color:'#69863C'}}></ShoppingBasketIcon>
+          <span className="option_line2  header_basket_content">0</span>
+
             
           </div>
         </nav>
