346fd569497a355b0ccdcf6a60af593a068a38dc
neeraj-gs <gsneeraj05@gmail.com>
2023-08-18 12:24:23 +0530

    Added a Basic Header Element or the nav bar

--- earth-cart/src/App.jsx
+++ earth-cart/src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
+    <div>
     <h1 className='app'>EarthCart</h1>
+    <Header />
+    </div>
   );
 }
 
--- earth-cart/src/Header.jsx
+++ earth-cart/src/Header.jsx
@@ -3,7 +3,36 @@ import './Header.css'
 
 function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+        <img className='header_logo' src="" alt="Earth Cart Logo" />
+        <div className="header_search">
+            <input type="text" className="header_search" />
+            {/* Logo for search we do using Material UI  */}
+        </div>
+        <nav className="header_nav">
+          <div className='header_option'>
+            <span className="option_line1">
+              Hello Guest
+            </span>
+            <span className="option_line2">
+              Sign In
+            </span>
+          </div>
+          <div className='header_option'>
+          <span className="option_line1">
+              Returns
+            </span>
+            <span className="option_line2">
+              & Orders
+            </span>
+            
+          </div>
+          <div className='header_option'>
+            {/* Cart Image */}
+            
+          </div>
+        </nav>
+    </div>
   )
 }
 
