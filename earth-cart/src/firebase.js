fd79ff8fdfb79c57893b9d7484bea4cba75347dd
neeraj-gs <gsneeraj05@gmail.com>
2023-08-18 11:43:34 +0530

    Created a new File firebase and added teh config code there to setup firebase to the project

---
+++ earth-cart/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAGooKOYr-GmvOSMgGiBaKHO3xERtpwRfY",
+    authDomain: "earth-clone.firebaseapp.com",
+    projectId: "earth-clone",
+    storageBucket: "earth-clone.appspot.com",
+    messagingSenderId: "888519621706",
+    appId: "1:888519621706:web:6550c60574575c5403bab3",
+    measurementId: "G-Y7LKYF1GJ6"
+  };
